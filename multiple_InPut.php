b08f3faad2062b9c35ac605625e945078090ef0b
PhyoTheinkha04 <kokha042@gmail.com>
2022-11-04 17:29:39 +0630

    Nov 4th-2

---
+++ multiple_InPut.php
@@ -0,0 +1,29 @@
+<?php
+
+if (isset($_POST['submit'])) {
+   $uname = $_POST['username'];
+   $email = $_POST['email'];
+   $pass = $_POST['password'];
+
+   echo $uname . " = "  . $email . " = " . $pass;
+
+  //$gender = $_POST['gender'];
+  //echo $gender;
+ }
+?>
+<form action="<?php $_PHP_SELF ?>" method="post" enctype="multipart/form-data">
+   <input type="text" name="username" placeholder="Username"><br><br>
+   <input type="email" name="email" placeholder="Email"><br><br>
+   <input type="password" name="password"><br><br>
+   <h3>Choose Gender</h3>
+   <input type="radio" name="gender" value="Male"><br><br>
+   <input type="radio" name="gender" value="Female"><br><br>
+   <h3>Choose Subject</h3>
+   <input type="checkbox" name="subjects[]" value="PHP">
+   <input type="checkbox" name="subjects[]" value="Ajax">
+   <input type="checkbox" name="subjects[]" value="JSON">
+   <input type="checkbox" name="subjects[]" value="MySQL"><br><br>
+   <input type="file" name="files[]" multiple><br><br>
+   <button type="sumit" name="submit">Register</button>
+
+</form>
